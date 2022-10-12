6f53aa715955e52771273d8da77de6c719f41765
Anderson Silva <andersonnsilva@alu.ufc.br>
2022-10-11 23:08:58 -0300

    App.js

--- primeira-avalicao-parcial/src/App.js
+++ primeira-avalicao-parcial/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+import { Questao01 } from "./components/Questao01";
+import { Questao01A } from "./components/Questao01A";
+import { Questao01B } from "./components/Questao01B";
+import { Questao02 } from './components/Questao02';
+import { Questao03 } from './components/Questao03';
+import { Questao04 } from './components/Questao04';
+import { Questao05 } from './components/Questao05';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='fs-3 mb-3 text-primary'>Questão 01</h1>
+      <div className='card mb-5 shadow p-3 mb-5 bg-body rounded'>
+        <Questao01>
+          <Questao01A />
+          <Questao01B />
+        </Questao01>
+      </div>
+      <h1 className='fs-3 mb-3 text-primary'>Questão 02</h1>
+      <div className='card shadow p-3 mb-5 bg-body rounded'>
+        <Questao02 />
+      </div>
+      <h1 className='fs-3 mb-3 text-primary'>Questão 03</h1>
+      <div className='card shadow p-3 mb-5 bg-body rounded'>
+        <Questao03 />
+      </div>
+      <h1 className='fs-3 mb-3 text-primary'>Questão 04</h1>
+      <div className='card shadow p-3 mb-5 bg-body rounded'>
+        <Questao04 />
+      </div>
+      <h1 className='fs-3 mb-3 text-primary'>Questão 05</h1>
+      <div className='card shadow p-3 mb-5 bg-body rounded'>
+        <Questao05 />
+      </div>
     </div>
   );
 }
